31117153687244301919bbbf5c65378167cc4148
mohdabidansari <meabid97@gmail.com>
2024-07-28 23:40:13 +0530

    Setup and build navbar

---
+++ src/components/global/navbar.tsx
@@ -0,0 +1,74 @@
+import { MenuIcon } from "lucide-react";
+import Link from "next/link";
+
+export const Navbar = () => {
+  const user = {};
+
+  return (
+    <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
+      <aside className="flex items-center gap-[2px]">
+        <svg
+          id="logo-38"
+          width="78"
+          height="32"
+          viewBox="0 0 78 32"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          {" "}
+          <path
+            d="M55.5 0H77.5L58.5 32H36.5L55.5 0Z"
+            className="ccustom"
+            fill="#FF7A00"
+          ></path>{" "}
+          <path
+            d="M35.5 0H51.5L32.5 32H16.5L35.5 0Z"
+            className="ccompli1"
+            fill="#FF9736"
+          ></path>{" "}
+          <path
+            d="M19.5 0H31.5L12.5 32H0.5L19.5 0Z"
+            className="ccompli2"
+            fill="#FFBC7D"
+          ></path>{" "}
+        </svg>
+      </aside>
+      <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
+        <ul className="flex items-center gap-4 list-none">
+          <li>
+            <Link href="#">Products</Link>
+          </li>
+          <li>
+            <Link href="#">Pricing</Link>
+          </li>
+          <li>
+            <Link href="#">Clients</Link>
+          </li>
+          <li>
+            <Link href="#">Resources</Link>
+          </li>
+          <li>
+            <Link href="#">Documentation</Link>
+          </li>
+          <li>
+            <Link href="#">Enterprise</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <aside className="flex items-center gap-4">
+        <Link
+          href="/dashboard"
+          className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            {user ? "Dashboard" : "Get Started"}
+          </span>
+        </Link>
+        {/* {user ? <UserButton afterSignOutUrl="/" /> : null} */}
+        <MenuIcon className="md:hidden" />
+      </aside>
+    </header>
+  );
+};
